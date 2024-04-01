c5b3010d71b5b469c7621d7de1d20cae58f8c127
JubaerHossain <jubaer01.cse@gmail.com>
2024-04-01 11:38:38 +0600

    updated by jubaer

--- static/public/assets/js/custom.js
+++ static/public/assets/js/custom.js
@@ -1,20 +1,49 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleModeButton = document.getElementById('toggleModeButton');
-    const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+  var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+  var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
-    // Check if a mode preference is stored
-    const modePreference = localStorage.getItem('modePreference');
-    if (modePreference) {
-        body.classList.add(modePreference);
+  // Change the icons inside the button based on previous settings
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!localStorage.getItem("color-theme") &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+    themeToggleLightIcon.classList.remove("hidden");
+  } else {
+    document.documentElement.classList.add("dark");
+    themeToggleDarkIcon.classList.remove("hidden");
+  }
+
+  console.log("localStorage.getItem('color-theme')", localStorage.getItem("color-theme"));
+
+  var themeToggleBtn = document.getElementById("theme-toggle");
+
+  themeToggleBtn.addEventListener("click", function () {
+    console.log("themeToggleBtn clicked");
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle("hidden");
+    themeToggleLightIcon.classList.toggle("hidden");
+
+    // if set via local storage previously
+    if (localStorage.getItem("color-theme")) {
+      if (localStorage.getItem("color-theme") === "light") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      }
+
+      // if NOT set via local storage previously
+    } else {
+      if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      }
     }
-
-    toggleModeButton.addEventListener('click', function () {
-        // Toggle between dark and white modes by toggling the body class
-        body.classList.toggle('dark-mode');
-        body.classList.toggle('white-mode');
-
-        // Store the mode preference in localStorage
-        const currentMode = body.classList.contains('dark-mode') ? 'dark-mode' : 'white-mode';
-        localStorage.setItem('modePreference', currentMode);
-    });
+  });
 });
